b489f932ad1ddcdaf35da309a934eecfdb1dc759
haval-ui <havalkoey00908@gmail.com>
2024-10-15 17:18:47 +0300

    october 15 commit

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -124,6 +124,56 @@ const RegisterForm = ({ user }: { user: User }) => {
           />
         </div>
 
+        <div className="flex flex-col gap-6 xl:flex-row ">
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="address"
+            label="Address"
+            placeholder="14th Street, New York"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="occupation"
+            label="Occupation"
+            placeholder="Softwear Engineer"
+          />
+        </div>
+        <div className="flex flex-col gap-6 xl:flex-row ">
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="emergencyContactName"
+            label="Emergency contact name"
+            placeholder="Guardian's name"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="emergencyContactNumber"
+            label="Emergency contact number"
+            placeholder="(555) 1234556 "
+          />
+        </div>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            {" "}
+            <h2 className="sub-header"> Medical Information </h2>
+          </div>
+        </section>
+
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+        <div className="flex flex-col gap-6 xl:flex-row "></div>
+
         <SubmitButton isLoading={isLoading}> Get Started </SubmitButton>
       </form>
     </Form>
